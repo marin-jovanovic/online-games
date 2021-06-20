3f10d72e71d3c81c43da0fa9f03181db251ea37d
marin-jovanovic <marin.jovanovic12@gmail.com>
2021-06-20 02:50:40 +0200

    :sparkles: add get and post methods

--- backend/app/views.py
+++ backend/app/views.py
@@ -5,4 +5,32 @@ from django.http import HttpResponse
 def home(req):
     print("home")
 
-    return render(req, "home.html")
+    return render(req, "home.html", {"temp_key": "temp value"})
+
+
+def add(req):
+    print("add")
+
+    if req.method == "POST":
+        print("post")
+
+        val1 = req.POST["firstname"]
+        val2 = req.POST["lastname"]
+
+        print(val1)
+        print(val2)
+
+        return render(req, "result.html", {"val1": val1, "val2": val2})
+
+    elif req.method == "GET":
+        print("GET")
+
+        val1 = "mitre"
+        val2 = "miric"
+
+        return render(req, "result.html", {"val1": val1, "val2": val2})
+
+
+    else:
+        print("OTHER")
+
